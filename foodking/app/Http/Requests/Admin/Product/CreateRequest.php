20c61b05ab0dd8a680022c289382c438a3793d9c
ninhchinguyenx <ninhchinguyenx@gmail.com>
2024-12-10 18:56:19 +0700

    Update CreateRequest.php

--- foodking/app/Http/Requests/Admin/Product/CreateRequest.php
+++ foodking/app/Http/Requests/Admin/Product/CreateRequest.php
@@ -23,27 +23,32 @@ class CreateRequest extends FormRequest
     {
         return [
             'name' => 'nullable|max:255',
-            'img' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'img_thumbnail' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
             'tags' => 'required',
             'category'=>'required',
-            'price_regular' => 'required',
-            'price_sale' => 'required',
-            'quantity' => 'required',
+            'price_regular' => 'required|numeric|min:0',
+            'price_sale' => [
+                'required',
+                'numeric',
+                'lt:price_regular',
+            ],
+            'quantity' => 'required|integer|min:0',
         ];
     }
     public function messages()
     {
         return [
             'name' => 'Tên không được để trống!',
-            'img.required' => 'Ảnh không được để trống!',
+            'img_thumbnail.required' => 'Ảnh không được để trống!',
             'tags.required' => 'Tags không được để trống!',
             'category.required' => 'Danh mục không được để trống!',
             'price_regular.required' => 'Giá không được để trống!',
             'price_sale.required' => 'Giá sale không được để trống!',
             'quantity.required' => 'Số lượng không được để trống!',
-            'img.image' => 'Tệp tải lên phải là ảnh!',
-            'img.mimes' => 'Ảnh phải có định dạng jpeg, png, jpg hoặc gif!',
-            'img.max' => 'Dung lượng ảnh không được vượt quá 2MB!',
+            'img_thumbnail.image' => 'Tệp tải lên phải là ảnh!',
+            'price_sale.lt' => 'Giá khuyến mãi phải nhỏ hơn giá gốc.',
+            'img_thumbnail.mimes' => 'Ảnh phải có định dạng jpeg, png, jpg hoặc gif!',
+            'img_thumbnail.max' => 'Dung lượng ảnh không được vượt quá 2MB!',
         ];
     }
 }
